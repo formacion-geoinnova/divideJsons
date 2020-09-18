4b6c90f9e5dce2a1230b0389488ef4deffa52828
erabasco <erabasco74@gmail.com>
2020-09-18 12:58:50 +0200

    last commit

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Divide Json</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="scripts.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
@@ -16,6 +18,12 @@
     <?php 
         session_start(); 
     ?>
+
+
+    <!-- spinner carga -->
+    <div class="loading show">
+        <div class="spin"></div>
+    </div>
     
     <div class="container text-center h-100 center-block justify-content-center align-items-center">
         <div class="mx-auto mt-5">
